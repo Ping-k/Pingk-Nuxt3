19d683095115423fb9934be9a02dda601216f50a
zhouyanping <17876995156@163.com>
2023-04-11 13:32:53 +0800

    @vueuse/core使用

--- pages/index.vue
+++ pages/index.vue
@@ -20,8 +20,15 @@
             </li>
         </ul>
     </div>
+
+    <button class="bg-red-400 mx-[20px] p-1 rounded border-solid border-4 border-light-red-500"
+        @click="toggleDark()">暗黑模式</button>{{ isDark }}
 </template>
-<script>
-export default {}
+
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
-<style lang=""></style>
+<style lang="scss"></style>
