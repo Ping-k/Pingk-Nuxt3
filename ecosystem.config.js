cb0fcfca07cb1ffd2ba013fede51979fb69d34b6
zhouyanping <17876995156@163.com>
2023-04-11 11:03:00 +0800

    自定义配置pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,17 @@
  * @Author: ZhouYanPing
  * @Date: 2023-04-11 10:38:58
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-04-11 10:44:22
+ * @LastEditTime: 2023-04-11 10:59:01
  */
 
 module.exports = {
   apps: [
     {
-      name: 'PingkNuxt',
-      port: '8082',
-      exec_mode: 'cluster',
-      instances: 'max',
-      script: './.output/server/index.mjs',
+      name: 'PingkNuxt', // 应用程序名称（默认为不带扩展名的脚本文件名）
+      port: '8082', // 端口
+      exec_mode: 'cluster', // 启动应用程序的模式，可以是“cluster”或“fork”，默认fork
+      instances: '1', // 要启动的应用实例数量，max 最大值
+      script: './.output/server/index.mjs', // 启动文件入口
     },
   ],
 }
