91aac507cc62ae3aee413ee324c580971d04123c
zhouyanping <17876995156@163.com>
2023-04-19 17:35:10 +0800

    使用definePageMeta和useHead动态配置标题

--- pages/layout.vue
+++ pages/layout.vue
@@ -4,7 +4,7 @@
  * @Author: ZhouYanPing
  * @Date: 2023-04-19 16:43:52
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-04-19 17:10:06
+ * @LastEditTime: 2023-04-19 17:34:30
 -->
 <template>
     <div>
@@ -12,8 +12,18 @@
     </div>
 </template>
 <script setup>
+/**
+ * 在你的pages/目录中，你可以使用definePageMeta和useHead来基于当前路由设置元数据。
+ */
+
 definePageMeta({
+    title: '自定义布局页面', // 默认标题，构建时提取到路由
     layout: "pingk", // 指定布局
 });
+
+const route = useRoute()
+useHead({
+    title: `App Name - ${route.meta.title}` // 动态标题，渲染时动态构建
+})
 </script>
 <style lang="scss"></style>
