a97b045ecda99be2b3b094bf2b0c771c37df6ec4
zhouyanping <17876995156@163.com>
2023-04-19 17:55:40 +0800

    修改多个根节点导致路由异常问题

--- pages/layout2.vue
+++ pages/layout2.vue
@@ -4,16 +4,18 @@
  * @Author: ZhouYanPing
  * @Date: 2023-04-19 16:43:52
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-04-19 17:11:46
+ * @LastEditTime: 2023-04-19 17:50:35
 -->
 
 <template>
-    <!-- 手动代码配置布局 -->
-    <NuxtLayout name="pingk">
-        <div>
-            pingk-布局-页面。2222。。
-        </div>
-    </NuxtLayout>
+    <div>
+        <!-- 手动代码配置布局 -->
+        <NuxtLayout name="pingk">
+            <div>
+                pingk-布局-页面。2222。。
+            </div>
+        </NuxtLayout>
+    </div>
 </template>
 <script setup>
 definePageMeta({
